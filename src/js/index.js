f4e54368436af4f5442b53d8c5d743d3f5213e29
Fernando Funez <ferfunes@gmail.com>
2019-08-22 00:10:10 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,37 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let generateExcuse = () => {
+    var who = ["the cop", "my 4geeks academy teacher", "his turtle", "my bird"];
+    var what = ["eat", "pissed", "crushed", "broked"];
+    var when = [
+      "before the class",
+      "right in time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+    var where = [
+      "on the street",
+      "in the park",
+      "at the office",
+      "in the train"
+    ];
+
+    var whoIndex = Math.floor(Math.random() * who.length);
+    var whatIndex = Math.floor(Math.random() * what.length);
+    var whenIndex = Math.floor(Math.random() * when.length);
+    var whereIndex = Math.floor(Math.random() * where.length);
+
+    return (
+      who[whoIndex] +
+      " " +
+      what[whatIndex] +
+      " " +
+      when[whenIndex] +
+      " " +
+      where[whereIndex]
+    );
+  };
+  document.querySelector("#excuse").innerHTML = generateExcuse();
 };
